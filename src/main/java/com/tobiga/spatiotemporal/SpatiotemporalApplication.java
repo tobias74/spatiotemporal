08263842b05fb677ec8c529b39d5846877746720
Tobias Gassmann <tobias@gaszmann.de>
2024-09-20 11:09:25 +0200

    initial commit

---
+++ src/main/java/com/tobiga/spatiotemporal/SpatiotemporalApplication.java
@@ -0,0 +1,13 @@
+package com.tobiga.spatiotemporal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpatiotemporalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpatiotemporalApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/tobiga/spatiotemporal/WebController.java
@@ -0,0 +1,15 @@
+package com.tobiga.spatiotemporal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("username", "Tobiga"); // Example dynamic value
+        return "index"; // Refers to src/main/resources/templates/index.html
+    }
+}
